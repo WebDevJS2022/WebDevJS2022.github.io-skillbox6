0390155906a06182e3bdf5b213ab948dab31aafd
WeDev <webdevjs2022@gmail.com>
2023-04-09 22:30:10 +0500

    Создание компонентов ProductList.vue и ProductItem.vue

---
+++ src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="catalog__list">
+          <ProductItem v-for="(product, index) in products" :key="index" :product="product"></ProductItem>
+        </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem';
+
+export default {
+    name: 'ProductList',
+    components: {ProductItem},
+    props: ['products'],
+}
+</script>
