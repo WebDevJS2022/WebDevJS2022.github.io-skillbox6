0faa24f8b3c678cc378767783bed307ab7e55e38
WeDev <webdevjs2022@gmail.com>
2023-04-17 17:05:48 +0500

    Пагинация Стрелки

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,11 +1,11 @@
 <template>
     <ul class="catalog__pagination pagination">
         <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow pagination__link--disabled" aria-label="Предыдущая страница">
+            <button class="pagination__link pagination__link--arrow" :disabled="page === 1" aria-label="Предыдущая страница" @click.prevent="prevPage()">
                 <svg width="8" height="14" fill="currentColor">
                     <use xlink:href="#icon-arrow-left"></use>
                 </svg>
-            </a>
+            </button>
         </li>
         <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
             <a href="#" class="pagination__link" :class="{'pagination__link--current': pageNumber === page}" @click.prevent="paginate(pageNumber)">
@@ -14,11 +14,11 @@
         </li>
  
         <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница">
+            <button class="pagination__link pagination__link--arrow" :disabled="page === 4" href="#" aria-label="Следующая страница" v-on:click.prevent="nextPage()">
                 <svg width="8" height="14" fill="currentColor">
                     <use xlink:href="#icon-arrow-right"></use>
                 </svg>
-            </a>
+            </button>
         </li>
     </ul>
 </template>
@@ -39,7 +39,13 @@ export default {
     methods: {
         paginate(page){
             this.$emit('paginate', page);
-        }
+        },
+        nextPage() {
+            this.$emit('paginate', (this.page + 1));
+        },
+        prevPage() {
+            this.$emit('paginate', (this.page - 1));
+        },
     }
 }
 </script>
