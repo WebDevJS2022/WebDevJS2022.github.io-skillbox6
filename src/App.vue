093ec652176b179b59cd53f62e6effb0fde0b815
WeDev <webdevjs2022@gmail.com>
2023-04-19 18:15:34 +0500

    Шина событий + упрощение переключения страниц

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <component :is="currentPageComponent" :page-params="currentPageParams" @gotoPage="(pageName, pageParams) => gotoPage(pageName, pageParams)" />
+  <component :is="currentPageComponent" :page-params="currentPageParams" />
 </template>
 
 <script>
 import MainPage from '@/pages/MainPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
+import eventBus from '@/eventBus';
 
 const routes = {  //список страниц, сюда добавляем новые страницы
   main: 'MainPage',
@@ -33,5 +34,9 @@ export default {
     }
   },
   components: {MainPage, ProductPage, NotFoundPage},
+  created() { //хук created
+    //добавляем обработчик события (в шину событий eventBus) переключения страниц
+    eventBus.$on('gotoPage', (pageName, pageParams) => this.gotoPage(pageName, pageParams))
+  }
 }
 </script>
