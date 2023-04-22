104b65ca87990eb009342fc16e3a4f1e25184513
WeDev <webdevjs2022@gmail.com>
2023-04-22 16:29:32 +0500

    6.3 Кнопки +/-

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -83,21 +83,8 @@
             </fieldset>
 
             <div class="item__row">
-              <div class="form__counter">
-                <button type="button" aria-label="Убрать один товар">
-                  <svg width="12" height="12" fill="currentColor">
-                    <use xlink:href="#icon-minus"></use>
-                  </svg>
-                </button>
-
-                <input type="text" v-model.number="productAmount">
-
-                <button type="button" aria-label="Добавить один товар">
-                  <svg width="12" height="12" fill="currentColor">
-                    <use xlink:href="#icon-plus"></use>
-                  </svg>
-                </button>
-              </div>
+              
+              <FormCounter v-model="productAmount" :amount="productAmount" />
 
               <button class="button button--primery" type="submit">
                 В корзину
@@ -165,8 +152,10 @@ import products from '@/data/products';
 import categories from '@/data/categories';
 import gotoPage from '@/helpers/gotoPage';
 import numberFormat from '@/helpers/numberFormat';
+import FormCounter from '@/components/FormCounter.vue';
 
 export default {
+  components: {FormCounter},
   data(){
         return {
             productAmount: 1
