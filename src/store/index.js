8373888bd043e8b337142e9afb6374dc57a790ee
WeDev <webdevjs2022@gmail.com>
2023-04-22 18:33:11 +0500

    6.5 Удаление товаров из корзины и изменение количества

--- src/store/index.js
+++ src/store/index.js
@@ -4,37 +4,47 @@ import products from '@/data/products';
 
 Vue.use(Vuex); //Сообщаем Vue, что будем использовать Vuex
 
-export default new Vuex.Store({  // Создаем и Экспортируем новое хранилище Vuex.Store
-  state: { // состояние товаров
-    cartProducts: [
-        {productId: 1, amount: 2}
-    ]
-  },
-  mutations: {
-    addProductToCart(state, {productId, amount}){
-        const item = state.cartProducts.find(item => item.productId === productId);
 
-        if(item){
-            item.amount += amount;
-        }else{
-            state.cartProducts.push({
-                productId,
-                amount,
-            });
-        }
-    }
-},
-getters: {
-    cartDetailProducts(state){ //подробная информация о товарах
-        return state.cartProducts.map(item => {
-            return {
-                ...item,
-                product: products.find(p => p.id === item.productId)
-            }
-        });
+export default new Vuex.Store({  // Создаем и Экспортируем новое хранилище Vuex.Store
+    state: { // состояние товаров
+      cartProducts: [
+          {productId: 1, amount: 2}
+      ]
     },
-    cartTotalPrice(state, getters){ //общая стоимость покупки
-        return getters.cartDetailProducts.reduce((acc, item) => (item.product.price * item.amount) + acc, 0);
+    mutations: {
+      addProductToCart(state, {productId, amount}){
+          const item = state.cartProducts.find(item => item.productId === productId);
+          if(item){
+              item.amount += amount;
+          }else{
+              state.cartProducts.push({
+                  productId,
+                  amount,
+              });
+          }
+      },
+      updateCartProductAmount(state, {productId, amount}){ //добавление товара в корзину
+          const item = state.cartProducts.find(item => item.productId === productId);
+  
+          if(item){
+              item.amount = amount;
+          }
+      },
+      deleteCartProduct(state, productId){ //удаление товара из корзины
+          state.cartProducts = state.cartProducts.filter(item => item.productId !== productId);
+      }
+    },
+    getters: {
+      cartDetailProducts(state){ //подробная информация о товарах
+          return state.cartProducts.map(item => {
+              return {
+                  ...item,
+                  product: products.find(p => p.id === item.productId)
+              }
+          });
+      },
+      cartTotalPrice(state, getters){ //общая стоимость покупки
+          return getters.cartDetailProducts.reduce((acc, item) => (item.product.price * item.amount) + acc, 0);
+      }
     }
-  }
-}); 
+  }); 
