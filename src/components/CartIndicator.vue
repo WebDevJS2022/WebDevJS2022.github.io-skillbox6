33b3f62d76d325823731ab2a01b1eb935a43c46d
WeDev <webdevjs2022@gmail.com>
2023-04-26 08:27:09 +0500

    7.7 Прелоадер загрузки корзины в Индикаторе корзины в шапке

--- src/components/CartIndicator.vue
+++ src/components/CartIndicator.vue
@@ -1,5 +1,7 @@
 <template>
-    <router-link class="header__cart" aria-label="Корзина с товарами" :to="{name: 'cart'}">
+    <router-link class="header__cart" aria-label="Корзина с товарами" :to="{name: 'cart'}" v-if="$store.state.cartLoading"><transition><img src="../img/Spinner-3.gif"></transition></router-link>
+    <router-link class="header__cart" aria-label="Корзина с товарами" :to="{name: 'cart'}" v-else-if="$store.state.cartLoadingFailed">Не удалось загрузить...</router-link>
+    <router-link class="header__cart" aria-label="Корзина с товарами" :to="{name: 'cart'}" v-else>
         <svg width="30" height="21" fill="currentColor">
             <use xlink:href="#icon-cart"></use>
         </svg>
