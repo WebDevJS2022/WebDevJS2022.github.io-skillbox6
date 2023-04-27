6a73d7b9bbc6965c752d1290a0a4c6db04ab11a8
WeDev <webdevjs2022@gmail.com>
2023-04-27 19:23:34 +0500

    8.1 Оформление заказа - Список товаров в правом блоке

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,15 @@
+<template>
+        <ul class="cart__orders">
+            <li class="cart__order">
+                <h3>{{ item.product.title }}</h3>
+                <b>{{ item.product.price }} ₽</b>
+                <span>Артикул: {{ item.product.id }}</span>
+            </li>
+        </ul>
+</template>
+
+<script>
+export default {
+    props: ['item'],
+}
+</script>
--- src/pages/OrderPage.vue
+++ src/pages/OrderPage.vue
@@ -23,7 +23,7 @@
         Корзина
       </h1>
       <span class="content__info">
-        3 товара
+        {{ $store.state.cartProducts.length }} товара
       </span>
     </div>
 
@@ -99,28 +99,13 @@
         </div>
 
         <div class="cart__block">
-          <ul class="cart__orders">
-            <li class="cart__order">
-              <h3>Смартфон Xiaomi Redmi Note 7 Pro 6/128GB</h3>
-              <b>18 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-            <li class="cart__order">
-              <h3>Гироскутер Razor Hovertrax 2.0ii</h3>
-              <b>4 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-            <li class="cart__order">
-              <h3>Электрический дрифт-карт Razor Lil’ Crazy</h3>
-              <b>8 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-          </ul>
-          
-          <div class="cart__total">
-            <p>Доставка: <b>500 ₽</b></p>
-            <p>Итого: <b>3</b> товара на сумму <b>37 970 ₽</b></p>
-          </div>
+
+            <OrderItem v-for="item in products" :key="item.productId" :item="item"/>
+
+            <div class="cart__total">
+                <p>Доставка: <b>500 ₽</b></p>
+                <p>Итого: <b>{{ $store.state.cartProducts.length }}</b> товара на сумму <b>{{ totalPrice | numberFormat }} ₽</b></p>
+            </div>
 
           <button class="cart__button button button--primery" type="submit">
             Оформить заказ
@@ -140,14 +125,21 @@
 <script>
 import BaseFormText from '@/components/BaseFormText.vue';
 import BaseFormTextarea from '@/components/BaseFormTextarea.vue';
+import OrderItem from '@/components/OrderItem.vue';
+import { mapGetters } from 'vuex';
+import numberFormat from '@/helpers/numberFormat';
 
 export default {
-    components: {BaseFormText, BaseFormTextarea},
+    components: {BaseFormText, BaseFormTextarea, OrderItem},
+    filters: {numberFormat},
     data(){
         return {
             formData: {}, //данные внесенные в формы
             formError: {}, //ошибки при заполнении формы
         }
+    },
+    computed: {
+        ...mapGetters({products: 'cartDetailProducts', totalPrice: 'cartTotalPrice'}), //totalPrice для обшей стоимости товара
     }
 }
 </script>
