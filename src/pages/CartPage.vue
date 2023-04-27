8fee4eaaf5c7d96fa3582ffa6f0eb676ecc57ea2
WeDev <webdevjs2022@gmail.com>
2023-04-27 18:13:10 +0500

    8.1 Показывать кнопку, если в корзине есть товары

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -42,7 +42,7 @@
                         Итого: <span>{{ totalPrice | numberFormat }} ₽</span>
                     </p>
 
-                    <router-link tag="button" class="cart__button button button--primery" type="submit" :to="{name: 'order'}">
+                    <router-link v-if="products.length > 0" tag="button" class="cart__button button button--primery" type="submit" :to="{name: 'order'}">
                         Оформить заказ
                     </router-link>
                 </div>
